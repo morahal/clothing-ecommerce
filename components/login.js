3aa5f6b23cb8cf1870ebd6d7f722ab84865b4ed5
Hadi <hadibazzi2019@gmail.com>
2024-05-16 04:00:24 +0300

    alert when pass/user incorrect

--- components/login.js
+++ components/login.js
@@ -1,13 +1,19 @@
 // LoginPage.js
-import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, TouchableOpacity, Alert } from 'react-native';
-import { BASE_URL } from '../constants';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import { BASE_URL } from "../constants";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const LoginPage = ({ navigation }) => {
-  const [username, setUserName] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
 
   // const validateUserName = (email) => {
   //   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -22,55 +28,49 @@ const LoginPage = ({ navigation }) => {
   // };
 
   const handleLogin = () => {
-
     const userData = {
-      "username": username,
-      "password": password,
-    }
+      username: username,
+      password: password,
+    };
 
     console.log(JSON.stringify(userData));
 
     const login = async (userData) => {
       try {
         const response = await fetch(`${BASE_URL}/login/`, {
-          method: 'POST',
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
           body: JSON.stringify(userData),
-          credentials: 'include',
+          credentials: "include",
         });
 
         if (!response.ok) {
-          throw new Error('HTTP error ' + response.status);
+          throw new Error("HTTP error " + response.status);
         }
 
         const jsonResponse = await response.json();
 
         if (jsonResponse.access) {
           // Store the access token upon successful login
-          await AsyncStorage.setItem('accessToken', jsonResponse.access);
+          await AsyncStorage.setItem("accessToken", jsonResponse.access);
           // Navigate to the Home screen or handle login success
           navigation.navigate("Home");
           console.log("You're logged in.");
-          
         } else {
-          console.log("Username or password incorrect");
+          alert("Username or password incorrect.");
+          console.log("Username or password incorrect.");
         }
-
-
+      } catch (err) {
+        console.error("Failed to login:", err);
       }
-      catch (err) {
-        console.error('Failed to login:', err);
-      }
-    }
+    };
 
     login(userData);
 
     // navigation.navigate("Home");
-
   };
-
 
   return (
     <View style={styles.container}>
@@ -93,12 +93,14 @@ const LoginPage = ({ navigation }) => {
       <TouchableOpacity style={styles.button} onPress={handleLogin}>
         <Text style={styles.buttonText}>LOGIN</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => { }}>
-        <Text style={styles.forgotPassword}>Have you forgotten your password?</Text>
+      <TouchableOpacity onPress={() => {}}>
+        <Text style={styles.forgotPassword}>
+          Have you forgotten your password?
+        </Text>
       </TouchableOpacity>
       <View style={styles.registerContainer}>
         <Text>Don't have an account? </Text>
-        <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
+        <TouchableOpacity onPress={() => navigation.navigate("SignUp")}>
           <Text style={styles.registerText}>Register</Text>
         </TouchableOpacity>
       </View>
@@ -110,49 +112,49 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    justifyContent: 'center',
-    backgroundColor: '#fff',
+    justifyContent: "center",
+    backgroundColor: "#fff",
   },
   logo: {
     fontSize: 45,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
     marginBottom: 48,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 24,
-    textAlign: 'center',
+    textAlign: "center",
   },
   input: {
     height: 40,
     borderBottomWidth: 1,
-    borderBottomColor: '#000',
+    borderBottomColor: "#000",
     marginBottom: 24,
   },
   button: {
-    backgroundColor: 'black',
+    backgroundColor: "black",
     padding: 12,
-    alignItems: 'center',
+    alignItems: "center",
     marginBottom: 24,
   },
   buttonText: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
   },
   forgotPassword: {
-    textAlign: 'center',
-    color: '#000',
+    textAlign: "center",
+    color: "#000",
     marginBottom: 24,
   },
   registerContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
   },
   registerText: {
-    fontWeight: 'bold',
-    color: 'black',
+    fontWeight: "bold",
+    color: "black",
   },
 });
 
--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
 // config.js
-const BASE_URL = "http://192.168.0.108:8000";
+const BASE_URL = "http://192.168.0.104:8000";
 export { BASE_URL };
